89b7922d0dc0205a1f116f094cc2f659c29f5958
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-04-30 09:45:10 +0530

    feat: onboarding form animation integration with motion

--- app/(web)/onboarding/collaborate/page.tsx
+++ app/(web)/onboarding/collaborate/page.tsx
@@ -1,6 +1,27 @@
 import { restrictOnboardingStep } from "@/app/actions/pages/onboarding/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { ChevronLeft } from "lucide-react"
+import Link from "next/link"
 
 export default async function CollaborationOnboardingPage() {
   await restrictOnboardingStep("collaborate")
-  return <div>Collaboration Onboarding page</div>
+  return (
+    <div className="w-full mx-auto max-w-lg flex items-center justify-center flex-col gap-y-3">
+      <div className="w-full flex gap-2 items-center">
+        <Link
+          href="/onboarding/workspace"
+          className={buttonVariants({
+            variant: "outline",
+            size: "icon",
+            className: "border-none",
+          })}
+        >
+          <ChevronLeft className="size-4" />
+        </Link>
+        <p className="text-xl font-semibold">Create your workspace</p>
+      </div>
+      {/* <WorkSpaceOnboardingForm /> */}
+      form
+    </div>
+  )
 }
--- components/forms/onboarding/workspace/index.tsx
+++ components/forms/onboarding/workspace/index.tsx
@@ -20,6 +20,7 @@ import {
 } from "@/lib/schema/pages/onboarding/workspace/workspace-onboarding-schema"
 import { clientEnv } from "@/lib/utilities/client-env"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { motion } from "motion/react"
 import { useForm } from "react-hook-form"
 
 interface WorkSpaceOnboardingFormProps {}
@@ -39,80 +40,92 @@ export const WorkSpaceOnboardingForm = ({}: WorkSpaceOnboardingFormProps) => {
   })
 
   return (
-    <Card className="w-full max-w-lg">
-      <CardHeader>
-        <ProfilePicComponent
-          maxSize={2 * 1024 * 1024}
-          title="Image"
-          fileSrc={form.getValues("logo")!}
-          accept="image/png,image/jpeg,image/jpg"
-          disabled={isPending}
-          onRemove={() => form.setValue("logo", "", formSetting)}
-          onSuccess={(imageSrc: string) => {
-            form.setValue("logo", imageSrc, formSetting)
-          }}
-        />
-      </CardHeader>
+    <motion.div
+      className="w-full max-w-lg"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          duration: 0.8,
+          ease: "easeInOut",
+        },
+      }}
+    >
+      <Card>
+        <CardHeader>
+          <ProfilePicComponent
+            maxSize={2 * 1024 * 1024}
+            title="Image"
+            fileSrc={form.getValues("logo")!}
+            accept="image/png,image/jpeg,image/jpg"
+            disabled={isPending}
+            onRemove={() => form.setValue("logo", "", formSetting)}
+            onSuccess={(imageSrc: string) => {
+              form.setValue("logo", imageSrc, formSetting)
+            }}
+          />
+        </CardHeader>
 
-      <CardContent className="mt-3">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit((values) => mutateAsync(values))}
-            className="space-y-6"
-          >
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Workspace Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      disabled={isPending}
-                      placeholder="example: Apple"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="slug"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Workspace URL</FormLabel>
-                  <FormControl>
-                    <div className="relative">
+        <CardContent className="mt-3">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit((values) => mutateAsync(values))}
+              className="space-y-6"
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Workspace Name</FormLabel>
+                    <FormControl>
                       <Input
-                        className="peer ps-30"
                         disabled={isPending}
-                        placeholder="example: apple"
+                        placeholder="example: Apple"
                         {...field}
                       />
-                      <span className="text-muted-foreground pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-sm peer-disabled:opacity-50">
-                        {new URL(clientEnv.NEXT_PUBLIC_APP_URL).host}/
-                      </span>
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <Button
-              disabled={isPending || !form.formState.isDirty}
-              type="submit"
-              className="w-full"
-              loading={isPending}
-            >
-              Create Workspace
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+              <FormField
+                control={form.control}
+                name="slug"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Workspace URL</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          className="peer ps-30"
+                          disabled={isPending}
+                          placeholder="example: apple"
+                          {...field}
+                        />
+                        <span className="text-muted-foreground pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-sm peer-disabled:opacity-50">
+                          {new URL(clientEnv.NEXT_PUBLIC_APP_URL).host}/
+                        </span>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                disabled={isPending || !form.formState.isDirty}
+                type="submit"
+                className="w-full"
+                loading={isPending}
+              >
+                Create Workspace
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </motion.div>
   )
 }
